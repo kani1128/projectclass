1166af471dfbabd8e4e54fbf64159520c4b8dd7f
kani1128 <harinidass1128@gmail.com>
2023-04-16 23:31:36 +0530

    Initial Codes

---
+++ src/test/java/com/Git/GittClass.java
@@ -0,0 +1,16 @@
+package com.Git;
+
+public class GittClass {
+private void method1() {
+	System.out.println("1");
+}
+private void method2() {
+	System.out.println("1");
+}
+private void method3() {
+	System.out.println("2");
+}
+private void method4() {
+	System.out.println("4");
+}
+}
