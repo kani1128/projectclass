a8f78124445ecb323b1a8bae844143e6efa8f0cd
kani1128 <harinidass1128@gmail.com>
2023-04-17 13:26:38 +0530

    codes

---
+++ src/test/java/com/Git/ProjectClass.java
@@ -0,0 +1,20 @@
+package com.Git;
+
+public class ProjectClass {
+
+	private void method1() {
+		System.out.println("1");
+	}
+	private void method2() {
+		System.out.println("1");
+	}
+	private void method3() {
+		System.out.println("2");
+	}
+	private void method4() {
+		System.out.println("4");
+	}
+	}
+	
+	
+
